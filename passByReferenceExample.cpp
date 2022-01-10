84a6a764e22af0e2588aee0dfa709fdcd983eeb4
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2022-01-10 14:03:42 -0800

    Create passByReferenceExample.cpp

---
+++ passByReferenceExample.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void fun(int &y);
+
+int main()
+{
+
+int x = 10;
+
+fun (x); // calling function by value of x
+
+cout << "x value in main = " << x << endl;
+return 0;
+}
+
+void fun(int &y)
+{
+    y++;
+    cout << "yvalue in main = " << y << endl;
+    return;
+}
