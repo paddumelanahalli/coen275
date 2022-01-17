3fb015003fbf690ed5d9d8f2fd94426ac7f55d1e
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2022-01-17 11:25:15 -0800

    Create 23-functionPointerExample.cpp

---
+++ 23-functionPointerExample.cpp
@@ -0,0 +1,30 @@
+// pointer to functions
+#include <iostream>
+using namespace std;
+
+int addition (int a, int b)
+{ return (a+b); }
+
+int subtraction (int a, int b)
+{ return (a-b); }
+
+int operation (int x, int y, int (*functoncall)(int,int))
+{
+  int g;
+  g = (*functoncall)(x,y);
+  return (g);
+}
+
+int main ()
+{
+  int m,n;
+  int (*minus)(int,int) = subtraction;
+
+  m = operation (7, 5, addition);
+  n = operation (20, m, minus);
+  cout << n;
+  return 0;
+}
+/*output
+8
+*/
