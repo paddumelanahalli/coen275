2b64ca5a1f723cd0d645db352c39a2fa2a6d8ce7
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2022-01-19 16:53:51 -0800

    Create 55-polymorphismAbstractBaseClass.cpp

---
+++ 55-polymorphismAbstractBaseClass.cpp
@@ -0,0 +1,45 @@
+// pure virtual members can be called
+// from the abstract base class
+#include <iostream>
+using namespace std;
+
+class Polygon {
+  protected:
+    int width, height;
+  public:
+    void set_values (int a, int b)
+      { width=a; height=b; }
+    virtual int area() =0;
+    void printarea()
+      { cout << this->area() << '\n'; }
+};
+
+class Rectangle: public Polygon {
+  public:
+    int area (void)
+      { return (width * height); }
+};
+
+class Triangle: public Polygon {
+  public:
+    int area (void)
+      { return (width * height / 2); }
+};
+
+int main () {
+  Rectangle rect;
+  Triangle trgl;
+  Polygon * ppoly1 = &rect;
+  Polygon * ppoly2 = &trgl;
+  ppoly1->set_values (4,5);
+  ppoly2->set_values (4,5);
+  ppoly1->printarea();
+  ppoly2->printarea();
+  return 0;
+}
+
+/*
+output
+20
+10
+*/
