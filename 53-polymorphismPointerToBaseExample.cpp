fe452fbca4f634692334e45fa5537db550601c0e
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2022-01-19 16:32:06 -0800

    Create 53-polymorphismPointerToBaseExample.cpp

---
+++ 53-polymorphismPointerToBaseExample.cpp
@@ -0,0 +1,41 @@
+// pointers to base class
+#include <iostream>
+using namespace std;
+
+class Polygon {
+  protected:
+    int width, height;
+  public:
+    void set_values (int a, int b)
+      { width=a; height=b; }
+};
+
+class Rectangle: public Polygon {
+  public:
+    int area()
+      { return width*height; }
+};
+
+class Triangle: public Polygon {
+  public:
+    int area()
+      { return width*height/2; }
+};
+
+int main () {
+  Rectangle rect;
+  Triangle trgl;
+  Polygon * ppoly1 = &rect;
+  Polygon * ppoly2 = &trgl;
+  ppoly1->set_values (4,5);
+  ppoly2->set_values (4,5);
+  cout << rect.area() << '\n';
+  cout << trgl.area() << '\n';
+  return 0;
+}
+
+/*
+output
+20
+10
+*/
