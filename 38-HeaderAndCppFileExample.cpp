c8462e1f081df3c22a00de48398c7a1212e1a4b3
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2022-01-28 20:14:15 -0800

    Update 38-HeaderAndCppFileExample.cpp

--- 38-HeaderAndCppFileExample.cpp
+++ 38-HeaderAndCppFileExample.cpp
@@ -1,52 +1,69 @@
 // Create a new folder in VS code and have only header and cpp files related to that program in this folder.
 // This will come handy for compiling all the cpp files in that folder with a common header file.
 
+// person.h
+#ifndef PERSON_H
+#define PERSON_H
+#include "date.h"
 
-
-// date.h file
-#ifndef DATE_H
-#define DATE_H
-
-
-#include <iostream>
-#include <cassert>
-using namespace std;
-
-class Date {
+class Person {
     private:
-        int month;
-        int day;
-        int year;
+        long identity;
+        Date birthDate; // object association
     public:
-        Date (int month, int day, int year);
-        ~Date();
+        Person(long id, Date birthdate);
+        ~Person();
         void print() const;
 };
 
 #endif
 
+//person.cpp
+#include "person.h"
 
-// date.cpp ( main cpp file)
-#include "date.h"
-
-Date::Date( int m, int d, int y) : month (m), day(d), year(y)
+Person::Person(long id, Date bd) : identity(id),birthDate(bd)
 {
- // Validation asserts can be added
+     // validation code
 }
 
-Date::~Date()
+Person::~Person()
 {
-
 }
 
-void Date::print() const
-{
-    cout << month <<"/" << day << "/"<< year <<endl;
+void Person::print() const{
+    cout << "Person id = " << identity << endl;
+    cout << "Person Date of Birth: " ;
+    birthDate.print();
+    cout << endl;
 }
+
+// main file
+#include "person.h"
+
 
 int main()
 {
+    Date date1 (5,6,1980);
+    Person person1 (1234, date1);
+    Date date2(4,23,1978);
+    Person person2(4567, date2);
+
+    //output
+    person1.print();
+    person2.print();
+    
     return 0;
 }
 
+
 // for compile this program - you will have to include all cpp files in the compiler option, you may have to edit the tasks.json file in VS Code.
+
+/* output
+Person id = 1234
+Person Date of Birth: 5/6/1980 
+
+Person id = 4567
+Person Date of Birth: 4/23/1978
+
+*/
+
